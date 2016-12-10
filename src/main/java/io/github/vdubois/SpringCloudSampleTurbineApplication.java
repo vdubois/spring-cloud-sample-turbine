b63bbd805105c08270bee0345a4fc3695f9772b1
Vincent Dubois <dubois.vct@free.fr>
2016-12-10 10:08:15 +0100

    [FIX] Turbine registration

--- src/main/java/io/github/vdubois/SpringCloudSampleTurbineApplication.java
+++ src/main/java/io/github/vdubois/SpringCloudSampleTurbineApplication.java
@@ -2,12 +2,12 @@ package io.github.vdubois;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableTurbine
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class SpringCloudSampleTurbineApplication {
 
 	public static void main(String[] args) {
